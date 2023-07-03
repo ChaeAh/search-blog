16732fddfad49829574d1a46bb6be6ce5b53d4ff
Chaeah <ejrgmla12@naver.com>
2023-07-03 22:36:47 +0900

    fix : 수정사항 반영

--- src/main/java/com/blog/kakao/config/WebClientConfig.java
+++ src/main/java/com/blog/kakao/config/WebClientConfig.java
@@ -3,10 +3,13 @@ package com.blog.kakao.config;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 /**
@@ -14,8 +17,10 @@ import reactor.netty.http.client.HttpClient;
  */
 
 @Configuration
+@Slf4j
 public class WebClientConfig {
 
+    /* webclient 생성 */
     @Bean
     public WebClient createWebClient() {
         HttpClient client = HttpClient.create()
@@ -25,6 +30,25 @@ public class WebClientConfig {
                         .addHandlerLast(new WriteTimeoutHandler(30)));
         return WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(client))
+                .filter(ExchangeFilterFunction.ofRequestProcessor(
+                        clientRequest -> {
+                            log.info("==============REQUEST==============");
+                            log.info("Request : {} {}", clientRequest.method(), clientRequest.url());
+                            clientRequest.headers().forEach(
+                                    (name, values) -> values.forEach(value -> log.info("{} : {}", name, value)));
+                            return Mono.just(clientRequest);
+                        }
+                ))
+                .filter(ExchangeFilterFunction.ofResponseProcessor(
+                        clientResponse -> {
+                            log.info("==============RESPONSE==============");
+                            clientResponse.headers().asHttpHeaders().forEach(
+                                    (name, values) -> values.forEach(value -> log.info("{} : {}", name, value)));
+                            return Mono.just(clientResponse);
+                        }
+                ))
+                .defaultHeader("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.3")
+                .defaultCookie("httpclient-type", "webclient")
                 .build();
     }
 
